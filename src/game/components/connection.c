78ff56083e51594c99e8a06c3cf18e1bf6046031
lilyyllyyllyly <lilylylith@gmail.com>
2024-10-29 14:38:12 -0300

    Fix whitespace

--- src/game/components/connection.c
+++ src/game/components/connection.c
@@ -8,27 +8,27 @@
 #include <stdio.h>
 
 void connection_update(component* c, game_data* data) {
-    float dist = Vector2Distance(c->owner->pos, c->connection.connected->pos);
-    if (dist > c->connection.max_dist) {
-        c->connection.connected->speed = 20;
+	float dist = Vector2Distance(c->owner->pos, c->connection.connected->pos);
+	if (dist > c->connection.max_dist) {
+		c->connection.connected->speed = 20;
 
 		if (data->princess_state == STATE_CALM)
-        	c->owner->pos = Vector2MoveTowards(c->owner->pos, c->connection.connected->pos, c->owner->speed * GetFrameTime());
-    }
-    else {
-        c->connection.connected->speed = 100;
-    }
+			c->owner->pos = Vector2MoveTowards(c->owner->pos, c->connection.connected->pos, c->owner->speed * GetFrameTime());
+	} else {
+		c->connection.connected->speed = 100;
+	}
 }
 
 void draw(component* c, game_data* data) {
-    DrawLineV(Vector2AddValue(c->owner->pos, 4), Vector2AddValue(c->connection.connected->pos, 4), c->connection.connection_colour);
+	DrawLineV(Vector2AddValue(c->owner->pos, 4), Vector2AddValue(c->connection.connected->pos, 4), c->connection.connection_colour);
 }
 
 component* connection_create(entity* connected, Color colour, float max_dist) {
-    component* c = component_create(TYPE_CONNECTION, connection_update, draw, NULL);
-    c->connection.connected = connected;
-    c->connection.connection_colour = colour;
-    c->connection.max_dist = max_dist;
+	component* c = component_create(TYPE_CONNECTION, connection_update, draw, NULL);
+	c->connection.connected = connected;
+	c->connection.connection_colour = colour;
+	c->connection.max_dist = max_dist;
 
-    return c;
+	return c;
 }
+
--- src/game/entities/princess.c
+++ src/game/entities/princess.c
@@ -2,8 +2,8 @@
 
 entity* princess_create(Vector2 position, entity* player) {
 	entity* princess = entity_create((Vector2){20, 10}, 40.0f);
-    entity_add_component(princess, rectangle_create((Vector2){8, 8}, PINK));
-    entity_add_component(princess, connection_create(player, BROWN, 40.0f));
+	entity_add_component(princess, rectangle_create((Vector2){8, 8}, PINK));
+	entity_add_component(princess, connection_create(player, BROWN, 40.0f));
 	entity_add_component(princess, princess_move_create(player));
 
 	return princess;

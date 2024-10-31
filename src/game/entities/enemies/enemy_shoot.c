9bca91b0559b5c5a7f85840a66c0e80e278e5e1e
lilyyllyyllyly <lilylylith@gmail.com>
2024-10-31 15:26:56 -0300

    Add shooting bat sprite

--- src/game/entities/enemies/enemy_shoot.c
+++ src/game/entities/enemies/enemy_shoot.c
@@ -8,7 +8,7 @@
 entity* enemy_shoot_create(scene* s, Vector2 position, float speed, scene* handler_scene, entity* princess) {
 	entity* enemy_shoot = entity_create(position, speed);
 
-	entity_add_component(enemy_shoot, sprite_create("assets/bat.png", (Vector2){-2, 0}, BLACK, false));
+	entity_add_component(enemy_shoot, sprite_create("assets/skullbat.png", (Vector2){-2, 0}, WHITE, false));
 	entity_add_component(enemy_shoot, constant_mover_create(speed, princess));
 
 	entity_add_component(enemy_shoot, bullet_handler_create(1.5f, handler_scene, princess));
